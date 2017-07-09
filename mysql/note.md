6fdfe0badee2fdbac5a77bda720b7db3fa559e15
root <38842860@qq.com>
2017-07-09 12:20:41 -0400

    db

--- mysql/note.md
+++ mysql/note.md
@@ -1,3 +1,16 @@
 # 问题记录
 ## 新安装的mysql出现ERROR! The server quit without updating PID file
    很有可能是配置文件里的sock路径没有权限
+
+## 在使用pymysql连接mysql时，需要制定字符集，否则会出现乱码
+具体连接的参数如下：
+
+param={
+'host':'',
+'port':...
+'user':...
+'password':...
+'charset':'utf8',
+'db':xxx
+}
+
--- stock/get_stocklist.py
+++ stock/get_stocklist.py
@@ -4,8 +4,13 @@
 # Date : 2017-7-9
 
 
+import sys
 import re
+
 import requests
+
+import pymysql
+
 
 url = 'http://quote.eastmoney.com/stocklist.html'
 res = requests.get(url)
@@ -18,6 +23,38 @@ pattern = r'<a target="_blank" href="http://quote.eastmoney.com/s[z|h)](\d+?).ht
 
 m = re.findall(pattern, res.text)
 
+#for item in m:
+#    if item[0].startswith(('3','6','0')):
+#        print item[0], item[1]
+
+
+# 1连接数据库
+dbparam = {
+    'host':'127.0.0.1',
+    'port':3306,
+    'user':'root',
+    'password':'',
+    'db':'stock',
+    'charset':'utf8'
+}
+
+conn = pymysql.connect(**dbparam)
+if not conn:
+    print '数据库连接错误'
+    sys.exit(-1)
+
+cursor = conn.cursor()
+#cursor.execute('select count(*) from stocks')
+#print cursor.fetchone()
+sql = u'insert into stocks(stockno, name) values '
 for item in m:
     if item[0].startswith(('3','6','0')):
-        print item[0], item[1]
+        s = u"('{}','{}'),".format(item[0],item[1])
+        sql += s
+sql = sql.strip(',')
+r = cursor.execute(sql.encode('utf8'))
+print r
+if r > 0:
+    conn.commit()
+cursor.close()
+conn.close()
